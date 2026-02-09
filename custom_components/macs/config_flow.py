32cf66533dc2385fc3e895f99d3419b5a6b819a6
Jonathan C. Dietrich <jcdietrich@gmail.com>
2026-02-09 13:46:03 -0500

    back out toggle

--- custom_components/macs/config_flow.py
+++ custom_components/macs/config_flow.py
@@ -1,8 +1,6 @@
 from __future__ import annotations
-import voluptuous as vol
 
 from homeassistant import config_entries
-from homeassistant.core import callback
 from homeassistant.data_entry_flow import FlowResult
 
 from .const import DOMAIN
@@ -10,36 +8,6 @@ from .const import DOMAIN
 class MacsConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
-    @staticmethod
-    @callback
-    def async_get_options_flow(config_entry: config_entries.ConfigEntry):
-        """Get the options flow for this handler."""
-        return MacsOptionsFlow(config_entry)
-
     async def async_step_user(self, user_input=None) -> FlowResult:
-        """Handle a flow initiated by the user."""
-        if self._async_current_entries():
-            return self.async_abort(reason="single_instance_allowed")
-        
-        return self.async_create_entry(title="MACS", data={})
-
-
-class MacsOptionsFlow(config_entries.OptionsFlow):
-    """Handle MACS options."""
-
-    async def async_step_init(self, user_input=None) -> FlowResult:
-        """Manage the MACS options."""
-        if user_input is not None:
-            return self.async_create_entry(title="", data=user_input)
-
-        return self.async_show_form(
-            step_id="init",
-            data_schema=vol.Schema(
-                {
-                    vol.Optional(
-                        "enable_shape_extensions",
-                        default=self.config_entry.options.get("enable_shape_extensions", False),
-                    ): bool
-                }
-            ),
-        )
+        # No options in V1; just create a single entry.
+        return self.async_create_entry(title="Macs", data={})
