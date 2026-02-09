c74cd82025b608e188eddaf146e5a14329d41870
Jonathan C. Dietrich <jcdietrich@gmail.com>
2026-02-09 12:21:13 -0500

    fix

--- custom_components/macs/config_flow.py
+++ custom_components/macs/config_flow.py
@@ -11,9 +11,6 @@ import voluptuous as vol
 from .const import DOMAIN
 
 class MacsOptionsFlowHandler(config_entries.OptionsFlow):
-    def __init__(self, config_entry):
-        self.config_entry = config_entry
-
     async def async_step_init(self, user_input=None):
         if user_input is not None:
             return self.async_create_entry(title="", data=user_input)
